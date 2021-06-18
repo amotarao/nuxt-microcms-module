27f352aa4eb1e2eb7a4d390c0ede4cea9159f3ce
Amon Sawamura <amon.sawamura@gmail.com>
2021-06-19 03:01:53 +0900

    rename change order

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,20 +1,32 @@
+import Vue from 'vue';
 import type { createClient } from 'microcms-js-sdk';
+
+interface MicrocmsModule {
+  client: ReturnType<typeof createClient>;
+}
 
 interface MicrocmsOptions {
   client: Parameters<typeof createClient>[0];
 }
 
-interface MicrocmsFunc {
-  client: ReturnType<typeof createClient>;
-}
-
-declare module '@nuxt/types' {
+declare module '@nuxt/vue-app' {
   interface Context {
-    $microcms: MicrocmsFunc;
+    $microcms: MicrocmsModule;
   }
 
   interface NuxtAppOptions {
-    $microcms: MicrocmsFunc;
+    $microcms: MicrocmsModule;
+  }
+}
+
+// Nuxt 2.9+
+declare module '@nuxt/types' {
+  interface Context {
+    $microcms: MicrocmsModule;
+  }
+
+  interface NuxtAppOptions {
+    $microcms: MicrocmsModule;
   }
 
   interface Configuration {
@@ -22,18 +34,8 @@ declare module '@nuxt/types' {
   }
 }
 
-declare module '@nuxt/vue-app' {
-  interface Context {
-    $microcms: MicrocmsFunc;
-  }
-
-  interface NuxtAppOptions {
-    $microcms: MicrocmsFunc;
-  }
-}
-
 declare module 'vue/types/vue' {
   interface Vue {
-    $microcms: MicrocmsFunc;
+    $microcms: MicrocmsModule;
   }
 }
